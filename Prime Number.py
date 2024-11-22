1e2e7e895c6759ba764b32c8d73533672e3bc95c
JyothiPriya5 <158424970+JyothiPriya5@users.noreply.github.com>
2024-11-22 12:01:28 +0530

    Day4

---
+++ Prime Number.py
@@ -0,0 +1,10 @@
+//Python code
+import math
+
+def isPrime(n: int) -> str:
+    if n<2:
+        return False
+    for i in range(2,int(math.sqrt(n))+1):
+        if n%i==0:
+            return False
+    return True          
