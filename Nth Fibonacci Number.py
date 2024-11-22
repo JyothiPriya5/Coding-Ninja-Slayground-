df6c6a74fbe519b19de546ad3d1346e382efcd5f
JyothiPriya5 <158424970+JyothiPriya5@users.noreply.github.com>
2024-11-22 11:56:15 +0530

    Nth Fibonacci Number.py

---
+++ Nth Fibonacci Number.py
@@ -0,0 +1,17 @@
+from math import *
+from collections import *
+from sys import *
+from os import *
+
+## Read input as specified in the question.
+## Print output as specified in the question.
+def Fibonacci(n):
+    if n==1 or n==2:
+        return 1
+    f1,f2=1,1
+    for _ in range(3,n+1):
+        f3=f1+f2
+        f1,f2=f2,f3
+    return f3  
+n=int(input())
+print(Fibonacci(n))
